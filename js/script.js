537c249b59bd4e4394885ea2fb8473ea4daaddcd
Luigi213 <reyesgianluigi88@gmail.com>
2022-11-25 15:25:12 +0100

    fixed the calculated and add the numberfixed

--- js/script.js
+++ js/script.js
@@ -4,19 +4,22 @@ let age = parseInt(prompt("Inserire l'età"));
 // 3- calcolo del prezzo del biglietto con le dovute le regole:
     // 3.1 applicare il prezzo in base ai chilometri da percorrere (0.21 € al km)
     let price_ticket = kilometres * 0.21;
+    let diff_discount;
     // 3.2- applicare lo sconto in base all'età
     if (age < 18){ 
         // 3.2.1- minorenni 20%
-        let discount_underage = price_ticket * 0.8;
-        price_ticket = discount_underage;
+        let discount_underage = price_ticket * 0.2;
+        diff_discount = price_ticket - discount_underage;
+        price_ticket = Number(diff_discount).toFixed(2);
     }
     else if (age > 17 && age < 66){
         price_ticket; 
     }
     else{
         // 3.2.1- over 65 di 40%
-        let discount_over65 = price_ticket * 0.6;
-        price_ticket = discount_over65;
+        let discount_over65 = price_ticket * 0.4;
+        diff_discount = price_ticket - discount_over65
+        price_ticket = Number(diff_discount).toFixed(2);
     }
 // 4- visualizza in forma umana 
 document.getElementById("ticket"). innerHTML = price_ticket + '€'
